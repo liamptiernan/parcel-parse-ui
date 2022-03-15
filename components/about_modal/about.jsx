59d9454b6f2f4c367ef8c9d5dafa077e3cd5ca0d
Liam Tiernan <liamptiernan@gmail.com>
2022-03-15 08:41:19 -0400

    clean about modal

--- components/about_modal/about.jsx
+++ components/about_modal/about.jsx
@@ -1,4 +1,9 @@
+import Image from 'next/image';
 import Modal from 'react-bootstrap/Modal';
+
+import selectPic from '../../public/select.png';
+import filterPic from '../../public/filter.png';
+
 
 function AboutModal(props) {
   return (
@@ -8,13 +13,15 @@ function AboutModal(props) {
           <Modal.Title>About ParcelParse</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>
+          <p className='lead border-bottom pb-3'>
             ParcelParse allows for the analysis of specific subsets of Monroe County property data.
           </p>
-          <h4>How to Use:</h4>
-          <p>Select a set of parcels to view. Current county sales are compiled into lists.</p>
-          <p>Use the filters to view specifc properites.</p>
-          <p>No warranties are made to the accuracy or completeness of this data.</p>
+          <h5 className='mb-3'>How to Use:</h5>
+          <Image src={selectPic} alt='Select Dropdown' width={244} height={32}/>
+          <p className='mt-3 mb-5'>Select a set of parcels to view. Lists are comprised of current county sales.</p>
+          <Image src={filterPic} alt='Select Dropdown' />
+          <p class="mt-4">Use the filters to view specifc properties.</p>
+          <p class="border-top pt-3 mt-4"><i>No warranties are made to the accuracy or completeness of this data.</i></p>
           <p><i><a href="https://github.com/liamptiernan" target="_blank" rel="noopener noreferrer">
             Developed by Liam Tiernan
           </a></i></p>
